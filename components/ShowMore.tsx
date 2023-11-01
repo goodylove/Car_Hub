9f8912a5a2118a8e4c5c3f8a287ad72fc7d6cbc5
goodness <goodyc474@gmail.com>
2023-11-01 20:36:23 +0100

    fix bug

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React from "react";
-import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { ShowMoreProps } from "@/Types";
 import { CustomButton } from ".";
